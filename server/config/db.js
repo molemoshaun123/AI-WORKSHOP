a0d606161efc6a7c59f0415fa5d67c3fb15dcc29
Molemo Shaun Kekana <molemoshaun373@gmail.com>
2026-08-25 01:27:09 +0200

    Fix database SSL connection for Neon.tech

--- server/config/db.js
+++ server/config/db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD || "",
   port: Number(process.env.DB_PORT),
+  ssl: process.env.DB_HOST !== 'localhost' ? { rejectUnauthorized: false } : false,
 });
 
 pool
